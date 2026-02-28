5c130e3443443de3523c7a2a597350004fbb64ab
Yeong-min Lee <133940530+cviplym@users.noreply.github.com>
2026-02-28 15:18:48 +0900

    Create 2667_그림.py

---
+++ baekjoon/dfs/2667_그림.py
@@ -0,0 +1,7 @@
+'''
+[Sources]
+Baekjoon: https://www.acmicpc.net/problem/2667
+
+[References]
+YouTube: https://www.youtube.com/watch?v=3_eVkTkBbJE&list=PLi-xJrVzQaxXC2Aausv_6mlOZZ2g2J6YB&index=3
+'''
